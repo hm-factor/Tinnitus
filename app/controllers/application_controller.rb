93c85a02e38b61d247fe055fff9d7a5505882427
Hunter <hsmanel@gmail.com>
2019-12-09 17:05:08 -0500

    application controller started

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+
+  end
+
+  def ensure_session_token
+
+  end
+
+  def login
+
+  end
+
+  def logout
+    @current_user.reset_session_token!
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
