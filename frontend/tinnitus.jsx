9f3720c8f7943f24e0ee277208eca121728608eb
Hunter <hsmanel@gmail.com>
2019-12-09 23:39:53 -0500

    Root all set to go

--- frontend/tinnitus.jsx
+++ frontend/tinnitus.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById('root');
   const store = configureStore();
-  ReactDOM.render(<Root store={store}/>, root)
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store}/>, root);
 
   window.signup = signup;
   window.login = login;
   window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
 })
