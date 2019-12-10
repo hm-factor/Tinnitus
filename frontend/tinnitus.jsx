10cb038a7a373b0a0f83893345b2b30f87af867f
Hunter <hsmanel@gmail.com>
2019-12-09 19:27:06 -0500

    api util complete

--- frontend/tinnitus.jsx
+++ frontend/tinnitus.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import configureStore from './store/store';
-// import { signup, login, logout } from './util/session_api_util';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   // const store = configureStore();
   ReactDOM.render(<h2>gah</h2>, root)
 
-  // window.signup = signup;
-  // window.login = login;
-  // window.logout = logout;
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 })
