f99b87c2b6ba97293ab5a07dcc5dd8a09cacb2d5
Hunter <hsmanel@gmail.com>
2020-02-18 20:54:33 -0500

    all icons for musicplayer added

--- frontend/components/music_player/music_player.jsx
+++ frontend/components/music_player/music_player.jsx
@@ -19,10 +19,18 @@ class MusicPlayer extends React.Component {
           {/* <i className="fas fa-heart"></i> */}
         </div>
         <div className="middle-musicplayer">
-
+          <i className="fas fa-random"></i>
+          <i className="fas fa-step-backward"></i>
+          <i className="fas fa-play"></i>
+          <i className="fas fa-pause"></i>
+          <i className="fas fa-step-forward"></i>
+          <i className="fas fa-repeat"></i>
         </div>
         <div className="right-side-musicplayer">
-          <i></i>
+          <i className="fas fa-volume-up"></i>
+          <i className="fas fa-volume-down"></i>
+          <i className="fas fa-volume-off"></i>
+          <i className="fas fa-volume-mute"></i>
         </div>
       </div>
     )
