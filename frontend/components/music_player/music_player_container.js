4dd648968e69ef2b36d8e56ee5aa52836f48767e
Hunter <hsmanel@gmail.com>
2020-02-19 23:49:42 -0500

    right side footer styling

--- frontend/components/music_player/music_player_container.js
+++ frontend/components/music_player/music_player_container.js
@@ -3,10 +3,10 @@ import MusicPlayer from './music_player';
 
 const msp = state => {
   return null;
-}
+};
 
 const mdp = dispatch => {
   return null;
-}
+};
 
 export default connect(msp, mdp)(MusicPlayer);
