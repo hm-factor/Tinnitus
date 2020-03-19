2e4cfd8958b99c9e27a0eeefb4a86b4e8316b17f
Hunter <hsmanel@gmail.com>
2020-03-19 13:36:27 -0400

    authroute fix

--- frontend/components/main_page/main_page_container.jsx
+++ frontend/components/main_page/main_page_container.jsx
@@ -3,7 +3,7 @@ import NavBarContainer from "../nav_bar/nav_bar_container";
 import SideBarContainer from '../side_bar/side_bar_container';
 import MusicPlayerContainer from '../music_player/music_player_container';
 
-import { Route, Link} from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import { ProtectedRoute, AuthRoute } from '../../util/route_util';
 import PlaylistShowContainer from "../music/playlist_show_container";
 // import SongIndexContainer from '../../components/music/song_index_container';
@@ -27,7 +27,8 @@ const MainPageContainer = state => {
           {/* <Route to='/' component={NavBarContainer} /> */}
         </div>
         <div className="playlist-show">
-          <Route path="/playlists/:id" component={PlaylistShowContainer} />
+          <Switch />
+            <AuthRoute path="/playlists/:id" component={PlaylistShowContainer} />
         </div>
       </div>
       <div className="main-bottom">
