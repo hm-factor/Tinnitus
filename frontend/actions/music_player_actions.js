91682f8bf0866d183937f9551243d35760570dce
Hunter <hsmanel@gmail.com>
2020-03-26 13:06:20 -0400

    musicplayer reducer and actions

---
+++ frontend/actions/music_player_actions.js
@@ -0,0 +1,22 @@
+export const PLAY_SONG = 'PLAY_SONG'; 
+export const PAUSE_SONG = 'PAUSE_SONG'; 
+export const TOGGLE = 'TOGGLE'; 
+
+export const playSong = song => {
+  return {
+    type: PLAY_SONG,
+    song
+  };
+};
+
+export const pauseSong = () => {
+  return {
+    type: PAUSE_SONG
+  }
+}
+
+export const togglePlay = () => {
+  return {
+    type: TOGGLE
+  }
+}
---
+++ frontend/reducers/music_player_reducer.js
@@ -0,0 +1,36 @@
+import {
+  PLAY_SONG,
+  PAUSE_SONG,
+  TOGGLE
+} from '../actions/music_player_actions';
+
+const emptyState = {
+  currentSong: {},
+  isPlaying: false,
+  songTime: 0,
+  songQueue: [],
+  songHistory: []
+};
+
+const musicPlayerReducer = (state = emptyState, action) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case PLAY_SONG:
+      newState.currentSong = action.song;
+      newState.isPlaying = true;
+      newState.songTime = action.song.duration;
+      newState.songHistory = newState.songHistory.push(action.song);
+      return newState;
+    case PAUSE_SONG:
+      newState.isPlaying = false;
+      return newState;
+    case TOGGLE:
+      newState.isPlaying = !state.isPlaying;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default musicPlayerReducer;
