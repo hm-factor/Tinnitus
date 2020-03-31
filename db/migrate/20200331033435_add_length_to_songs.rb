512559ddaa4a640944f51366ae8f249d543e671c
Hunter <hsmanel@gmail.com>
2020-03-30 23:36:35 -0400

    added length to song table

---
+++ db/migrate/20200331033435_add_length_to_songs.rb
@@ -0,0 +1,5 @@
+class AddLengthToSongs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :songs, :length, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_010806) do
+ActiveRecord::Schema.define(version: 2020_03_31_033435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_010806) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "length"
     t.index ["album_id"], name: "index_songs_on_album_id"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
